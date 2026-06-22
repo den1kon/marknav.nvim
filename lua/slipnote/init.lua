ca4eb4c2e0b466cb4808e317bbb86e779eec997f
Denys Kondratiuk <dkondratiuk@pm.me>
2026-06-22 19:25:02 +0200

    [#6] fix: cleanup init.lua

--- lua/slipnote/init.lua
+++ lua/slipnote/init.lua
@@ -9,7 +9,6 @@ local M = {}
 ---@param user_config? SlipnoteConfig
 function M.setup(user_config)
 	local config = Config.resolve(user_config)
-	local frontmatter_config = Config.get_frontmatter(config)
 
 	vim.api.nvim_create_user_command("FollowLink", CmdHandler.follow_link, { desc = "Follow link at cursor" })
 	vim.api.nvim_create_user_command("FollowBack", CmdHandler.follow_back, { desc = "Go back to previos buffer" })
@@ -18,14 +17,6 @@ function M.setup(user_config)
 		CmdHandler.follow_link_in_new_tab,
 		{ desc = "Follow link at cursor in new tab" }
 	)
-
-	if Config.frontmatter_enabled(config) then
-		vim.api.nvim_create_user_command(
-			"InsertFrontmatter",
-			CmdHandler.insert_frontmatter,
-			{ desc = "Insert YAML frontmatter" }
-		)
-	end
 
 	local augroup = vim.api.nvim_create_augroup("SlipnoteAutocommands", { clear = true })
 
@@ -39,13 +30,7 @@ function M.setup(user_config)
 		end,
 	})
 
-	vim.api.nvim_create_autocmd("BufWritePre", {
-		group = augroup,
-		callback = function(args)
-			Frontmatter.update_frontmatter_updated_at(args.buf)
-		end,
-	})
-
+  Frontmatter.setup(config, augroup)
 	Conceal.setup(config, augroup)
 end
 
